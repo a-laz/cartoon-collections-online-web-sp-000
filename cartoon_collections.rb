e448edabe95791cb0f7ec79fdaf62aa52b79897c
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-07 04:37:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,11 @@ def long_planeteer_calls(array)# code an argument here
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.detect do |snack| 
+    return snack if cheese_types.include?(snack)
+  end
+  nil
 end
