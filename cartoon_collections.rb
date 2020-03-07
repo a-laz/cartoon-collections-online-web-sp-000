0b739e43fd8ec72e9ee2930903ce5107c33b0c3e
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-07 04:32:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,12 @@ def summon_captain_planet(array)# code an argument here
   array.collect {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.detect do |element|
+    return false if element.length > 4
+  end
+  true 
 end
 
 def find_the_cheese# code an argument here
